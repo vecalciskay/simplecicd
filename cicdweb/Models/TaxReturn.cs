e3799ee2a5f355431c16f9b55d199203447d1563
Vladimir <vladimir.calderon@gmail.com>
2023-05-30 09:14:33 -0400

    Just changed the title

---
+++ cicdweb/Models/TaxReturn.cs
@@ -0,0 +1,44 @@
+﻿namespace cicdweb.Models
+{
+    /// <summary>
+    /// This class represents a tax return of a tax payer.
+    /// Objects of this class have the taxpayer, the amount, the period and the duedate.
+    /// A penalty fee is applied if the tax return is not paid until the duedate.
+    /// </summary>
+    public class TaxReturn
+    {
+        public TaxPayer TaxPayer { get; set; }
+        public double Amount { get; set; }
+        public int Period { get; set; }
+        public DateTime DueDate { get; set; }
+        public double Penalty { get; set; }
+
+        public TaxReturn(TaxPayer taxPayer, double amount, int period, DateTime duedate)
+        {
+            TaxPayer = taxPayer;
+            Amount = amount;
+            Period = period;
+            DueDate = duedate;
+        }
+
+        /// <summary>
+        /// A penalty fee is applied if the tax return is not paid until the duedate.
+        /// The penalty is calculated as 2 for each day passed after the duedate.
+        /// </summary>
+        public void CalculatePenalty()
+        {
+            if (DateTime.Now > DueDate)
+            {
+                Penalty = (DateTime.Now - DueDate).Days * 2;
+            }
+        }
+
+        public void Pay()
+        {
+            if (Penalty > 0)
+            {
+                Amount += Penalty;
+            }
+        }
+    }
+ }
---
+++ cicdweb/Models/Taxpayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Cryptography;
+
+namespace cicdweb.Models
+{
+    /// <summary>
+    /// This class represents a tax payer.
+    /// The taxpayer has a name and a tax id, and a list of tax returns.
+    /// 
+    /// </summary>
+    public class TaxPayer
+    {
+        public string Name { get; set; }
+        public string TaxId { get; set; }
+        public List<TaxReturn> TaxReturns { get; set; }
+
+        public TaxPayer(string name, string taxId)
+        {
+            Name = name;
+            TaxId = taxId;
+            TaxReturns = new List<TaxReturn>();
+        }
+
+        public void AddTaxReturn(TaxReturn taxReturn)
+        {
+            TaxReturns.Add(taxReturn);
+        }
+
+        public void RemoveTaxReturn(TaxReturn taxReturn)
+        {
+            TaxReturns.Remove(taxReturn);
+        }
+
+        public void PayTaxReturn(TaxReturn taxReturn)
+        {
+            taxReturn.CalculatePenalty();
+            taxReturn.Pay();
+        }
+
+        public void PayAllTaxReturns()
+        {
+            foreach (TaxReturn taxReturn in TaxReturns)
+            {
+                taxReturn.CalculatePenalty();
+                taxReturn.Pay();
+            }
+        }
+        public void PayOnlyTaxReturnThatAreDue()
+        {
+            foreach (TaxReturn taxReturn in TaxReturns)
+            {
+                if (taxReturn.DueDate < DateTime.Now)
+                {
+                    taxReturn.CalculatePenalty();
+                    taxReturn.Pay();
+                }
+            }
+        }
+    }
+}
