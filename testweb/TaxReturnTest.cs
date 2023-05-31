eb4c4864342739f65216d5296fa44df8ce5fa135
Vladimir <vladimir.calderon@gmail.com>
2023-05-31 04:45:00 -0400

    Added test for calculation

---
+++ testweb/TaxReturnTest.cs
@@ -0,0 +1,24 @@
+﻿using cicdweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testweb
+{
+    public class TaxReturnTest
+    {
+        [Fact]
+        public void TestCalculation()
+        {
+            DateTime tenDaysAgo = DateTime.Now.AddDays(-10);
+            TaxPayer taxPayer = new TaxPayer("Vladimir", "HU23425");
+            TaxReturn taxReturn = new TaxReturn(taxPayer, 100, 2018, tenDaysAgo);
+
+            taxReturn.CalculatePenalty();
+
+            Assert.Equal(20, taxReturn.Penalty);
+        }
+    }
+}
